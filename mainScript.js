2fb1ef96018d2dfa147d37f0a7540600034c0604
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-03 14:32:37 +0100

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -3,6 +3,7 @@ const signupScreen = document.getElementById("signup")
 const loginScreenBtn = document.getElementById("loginBtn")
 const signupScreenBtn = document.getElementById("signupBtn")
 
+loginScreen.style.display = "";
 loginScreenBtn.onclick = function(){
   loginScreen.style.display = "";
   signupScreen.style.display = "none";
