1b257a8704ce1d289db4469d5dbd56156aa42511
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-07 16:17:40 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST["submit"])) {
+if(isset($_POST["submit"] && $_POST['Iusername'] !== "" && $_POST['Ipassword'] !== "" && $_POST['Iconfpassword'] !== "")) {
   echo "It works";
 }
 else {
