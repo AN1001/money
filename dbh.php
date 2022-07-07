2c9de7e798e50fe7595584d47fb3571f76d6610c
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-07 20:16:09 +0100

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -3,5 +3,6 @@
 $conn = pg_connect(getenv("postgres://famzfecldkdbgu:0ed5a04607d1f3e5009c30f8f7e26b54daa47d3d4a3cd96e65466f78ed951755@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/d7lb9lcj3j0p16"));
 
 if (!$conn) {
+  $DATABASE_URL = getenv("DATABASE_URL");
   die("it seems like------error");
 }
