0ffcd2a0467571b3b6d966de40e5ddda41f0552f
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-07 17:02:17 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,8 +1,16 @@
 <?php
 
 if(isset($_POST["submit"]) && !empty($_POST['Iusername']) && !empty($_POST['Ipassword']) && !empty($_POST['Iconfpassword']) ) {
-  echo "It works";
+  
+  $username = $_POST["Iusername"];
+  $password = $_POST["Ipassword"];
+  $confpassword = $_POST["Iconfpassword"];
+  
+  require_once 'dbh.php';
+  
+  exit();
 }
 else {
-  header("location: main.html");
+  header("location: main.html?error=emptyinput");
+  exit();
 }
