3accf0010e185c60a34c492113417c1659b9abc5
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-07 19:17:40 +0100

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -19,5 +19,22 @@ signupScreenBtn.onclick = function(){
   signupScreen.style.display = "";
 };
 
-var errorType = (window.location.href+"?none").split("?")[1]
-console.log(errorType)
+var errorType = (window.location.href+"?none").split("?")[1];
+if (errorType != "none"):
+  var errorReigon = '';
+  errorType = errorType.slice(6);
+  errorReigon = errorType.slice(0,2);
+  errorType = errorType.slice(4);
+  console.log(errorType, errorReigon);
+  if errorType == "emptyinput"{
+    if errorReigon == "LI" {loginError.textContent = "Error not all fields filled in";
+                           } else {signupError.textContent = "Error not all fields filled in";}
+  } else if errorType == "nopwdmatch" {
+    if errorReigon == "LI" {loginError.textContent = "Error passwords do not match";
+                           } else {signupError.textContent = "Error passwords do not match";}
+  } else if errorType == "uidtaken" {
+    if errorReigon == "LI" {loginError.textContent = "Error username already in use";
+                           } else {signupError.textContent = "Error username already in use";}
+  }
+  
+  
