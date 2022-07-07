13b49e6e6bdabeb6cad4900931dba0bf3ba592ce
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-07 20:31:47 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,38 @@
 <?php
 
 if(isset($_POST["submit"]) && !empty($_POST['Iusername']) && !empty($_POST['Ipassword'])) {
-  echo "It works";
+  
+  
+  function incorrectPwd($password,$confpassword) {
+    $result = false;
+    return $result;
+  }
+  
+  function incorrectUid($conn, $username) {
+    $result = false;
+    return $result;
+  }
+  
+  function logIn($conn, $username, $password) {
+    echo "login with credentials";
+    $a;
+  }   
+            
+  
+  
+  if (incorrectPwd($conn, $password) !== false) {
+    header("location: main.html?error=LI-nopwdmatch");
+    exit();
+  }
+  if (incorrectUid($conn, $username) !== false) {
+    header("location: main.html?error=LI-uidtaken");
+    exit();
+  }
+  
+  logIn($conn, $username, $password);
+  
 }
 else {
-  header("location: main.html");
+  header("location: main.html?error=LI-emptyinput");
+  exit();
 }
