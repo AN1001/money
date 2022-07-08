ab2f952ad0ff92becf569f6f323f884adf27807f
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-08 17:49:17 +0100

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,6 +1,5 @@
 <?php
 
-echo getenv("DATABASE_URL");
 $conn = pg_connect("host=ec2-54-246-185-161.eu-west-1.compute.amazonaws.com port=5432 dbname=d7lb9lcj3j0p16 user=famzfecldkdbgu
  password=0ed5a04607d1f3e5009c30f8f7e26b54daa47d3d4a3cd96e65466f78ed951755");
 
