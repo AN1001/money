ae48fe2dc68c4bef1e500e353136441dcf522943
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-08 12:54:44 +0100

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,9 +1,11 @@
 <?php
 
-$conn = pg_connect(getenv("postgres://famzfecldkdbgu:0ed5a04607d1f3e5009c30f8f7e26b54daa47d3d4a3cd96e65466f78ed951755@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/d7lb9lcj3j0p16"));
+$conn = pg_connect(getenv("DATABASE_URL"));
 
 if (!$conn) {
   $DATABASE_URL = getenv("DATABASE_URL");
   echo $DATABASE_URL;
   die("it seems like------error");
+} else {
+  echo "Connection successful";
 }
