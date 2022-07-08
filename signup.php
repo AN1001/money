dccab12c00d2efa7f778ca61718d0c28dda281e0
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-08 16:21:27 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -55,6 +55,8 @@ if(isset($_POST["submit"]) && !empty($_POST['Iusername']) && !empty($_POST['Ipas
   
   $sql = "SELECT * FROM userinfo WHERE username LIKE '$username';";
   $dbresult = pg_query($conn, $sql);
+  echo "123"
+  echo $dbresult
   header("location: main.html?$dbresult");
   //createUser($conn, $username, $password);
   
