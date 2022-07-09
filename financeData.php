15f614008939d6c7b3a69244789d1f766c7d4f39
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-09 22:13:37 +0100

    Update and rename financeData.php to graphDataTransfer.php

--- financeData.php
+++
@@ -1,3 +0,0 @@
-<?php
-session_start();
-$dbGraphData = $_SESSION['graphData'];
---
+++ graphDataTransfer.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+$dbGraphData = $_SESSION['graphData'];
+?>
+
+<script type="text/javascript">
+    var graphDataRaw = "<?php echo $time; ?>";
+</script>
