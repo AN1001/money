f4e2fd8de3edb95ede3c15d2d1bb289dcea6c728
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-10 09:42:18 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 if(isset($_POST["submit"]) && !empty($_POST['Iusername']) && !empty($_POST['Ipassword'])) {
   
   $username = $_POST["Iusername"];
@@ -41,11 +43,9 @@ if(isset($_POST["submit"]) && !empty($_POST['Iusername']) && !empty($_POST['Ipas
       $dbGraphData = pg_fetch_row($dbresultGraphs)[0];
       echo pg_last_error($conn);
       echo $dbGraphData;
-      echo gettype($dbGraphData);
       echo "user logged in";
-      setcookie("graphData=" . $dbGraphData);
-      echo "usoggn";
-      
+
+      $_SESSION['graphData'] = $dbGraphData;
       header("location: financeDisplay.html?$dbGraphData");
       exit();
     }
