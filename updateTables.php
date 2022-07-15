7eccf5629863af6bd0090d355b08b2048ba5dc46
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-15 12:28:27 +0100

    Update updateTables.php

--- updateTables.php
+++ updateTables.php
@@ -1,1 +1,7 @@
+<?php
+require_once 'dbh.php';
+$username = $_COOKIE["username"];
+$graphData = $_COOKIE["graphData"];
 
+$tableUpdate = "UPDATE userinfo SET graphdata = '$graphData' WHERE username = '$username'";
+pg_query($conn, $tableUpdate);
