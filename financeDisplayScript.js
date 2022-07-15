97495fbac66a45c700a45a24d72151fcb17f8df7
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-15 22:06:00 +0100

    Update financeDisplayScript.js

--- financeDisplayScript.js
+++ financeDisplayScript.js
@@ -80,7 +80,7 @@ logOutBtn.addEventListener("click",function(){
 		dataAddScreen.style.display = "none";
 		changeGraphDataParent.style.display = "none"
 		
-		changeGraphArea.forEach(function(barStats){
+		changeGraphArea.children.forEach(function(barStats){
 			barStats.remove();
 		})
 	}
