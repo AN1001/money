559c82c3d9ae94d54dd4912f84140ed178ca8e7a
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-07-17 13:22:08 +0100

    Update login.php

--- login.php
+++ login.php
@@ -42,7 +42,7 @@ if(isset($_POST["submit"]) && !empty($_POST['Iusername']) && !empty($_POST['Ipas
       echo pg_last_error($conn);
       echo $dbGraphData;
 
-      setcookie("graphData", $dbGraphData, time()+10, '/');
+      setcookie("graphData", $dbGraphData, time()+600, '/');
       echo "user logged in";
 
       header("location: financeDisplay.html");
